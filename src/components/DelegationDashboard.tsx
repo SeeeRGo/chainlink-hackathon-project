003e9f106610d8c3c2f6debef289d8f06ca097bc
Sergey Mizin <seeergo@yahoo.com>
2022-05-13 19:23:19 +0400

    removed self from delegate options

--- src/components/DelegationDashboard.tsx
+++ src/components/DelegationDashboard.tsx
@@ -7,39 +7,42 @@ interface Props {
   users: DelegationGraph;
   onToggleDelegate: (
     userId: Governor["id"],
-    delegateId: Governor["id"],
+    delegateId: Governor["id"]
   ) => void;
 }
 
-export const DelegationDashboard = ({
-  users,
-  onToggleDelegate,
-}: Props) => {
+export const DelegationDashboard = ({ users, onToggleDelegate }: Props) => {
   const account = useAccount();
 
-  const delegates: Governor['delegates'] = account ? users[account]?.delegates ?? [] : [];
-  const followers: Governor['followers'] = account ? users[account]?.followers ?? [] : [];
+  const delegates: Governor["delegates"] = account
+    ? users[account]?.delegates ?? []
+    : [];
+  const followers: Governor["followers"] = account
+    ? users[account]?.followers ?? []
+    : [];
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       <div>
         <h3>Your Delegates</h3>
         <FormGroup>
-          {Object.values(users).map((governor) => (
-            <FormControlLabel
-              control={
-                <Checkbox
-                  value={governor.id}
-                  checked={delegates.includes(governor.id)}
-                  onChange={() => {
-                    if(account) {
-                      onToggleDelegate(account, governor.id);
-                    }
-                  }}
-                />
-              }
-              label={governor.name}
-            />
-          ))}
+          {Object.values(users)
+            .filter((governor) => governor.id !== account)
+            .map((governor) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value={governor.id}
+                    checked={delegates.includes(governor.id)}
+                    onChange={() => {
+                      if (account) {
+                        onToggleDelegate(account, governor.id);
+                      }
+                    }}
+                  />
+                }
+                label={governor.name}
+              />
+            ))}
         </FormGroup>
         <div style={{ display: "flex", flexDirection: "row" }}>
           <Button>Save</Button>
