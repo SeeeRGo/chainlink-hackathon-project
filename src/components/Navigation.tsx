38c6f2019c53543ba0749700f5467c09aa94fac4
Sergey Mizin <seeergo@yahoo.com>
2022-05-18 22:36:33 +0400

    styled navigation menu and revorked routing

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,30 @@
+import { Box } from '@material-ui/core';
+import { Button } from '@mui/material';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const Navigation = () => (
-  <div>
-    <a href='/vote'>Voting Booth</a>
-    <br />
-    <a href='/delegate'>Delegation Dashboard</a>   
-    <br />
-    <a href='/results'>Voting results</a>
-  </div>
-)
+const pages = [
+  ["Voting Booth", "/vote"],
+  ["Delegation Dashboard", "/delegate"],
+  ["Voting results", "/results"],
+];
+export const Navigation = () => {
+  const navigate = useNavigate();
+  return (
+    <Box sx={{ flexGrow: 1, display: { md: "flex" } }}>
+      {pages.map((page, i) => (
+        <Button
+          key={page[0] ?? `${i}`}
+          onClick={() => {
+            if(page[1]) {
+              navigate(page[1]);
+            }
+          }}
+          sx={{ my: 2, display: "block" }}
+        >
+          {page[0]}
+        </Button>
+      ))}
+    </Box>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { DelegationDashboard } from "./components/DelegationDashboard";
 import { Navigation } from "./components/Navigation";
 import { VotingBooth } from "./components/VotingBooth";
@@ -8,38 +8,48 @@ import { initialState } from "../tests/fixtures/votingState.fixtures";
 import { DelegationGraph } from "./types/delegationGraph";
 import { getDelegationGraph, updateDelegationGraph } from "./api";
 
+const Layout = () => {
+  return (
+    <main>
+      <Navigation />
+      <Outlet />
+    </main>
+  );
+};
+
 export const App = () => {
   const [graph, setGraph] = useState<DelegationGraph>({});
   const getData = useCallback(async () => {
-    getDelegationGraph().then(data => setGraph(data))
-  }, [])
+    getDelegationGraph().then((data) => setGraph(data));
+  }, []);
   useEffect(() => {
     getData();
   }, []);
-  
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigation />} />
-        <Route path="/vote" element={<VotingBooth />} />
-        <Route
-          path="/delegate"
-          element={
-            <DelegationDashboard
-              users={graph}
-              onToggleDelegate={async (userId, delegateId) => {
-                await updateDelegationGraph(userId, delegateId);
-                await getData();
-              }}
-            />
-          }
-        />
-        <Route
-          path="/results"
-          element={
-            <Results delegationGraph={graph} votingState={initialState} />
-          }
-        />
+        <Route path="/" element={<Layout />}>
+          <Route path="vote" element={<VotingBooth />} />
+          <Route
+            path="delegate"
+            element={
+              <DelegationDashboard
+                users={graph}
+                onToggleDelegate={async (userId, delegateId) => {
+                  await updateDelegationGraph(userId, delegateId);
+                  await getData();
+                }}
+              />
+            }
+          />
+          <Route
+            path="results"
+            element={
+              <Results delegationGraph={graph} votingState={initialState} />
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
